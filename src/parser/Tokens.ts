2dd96c70083b405746ffeca3533861bf3d84e8c8
valentin11 <valentinfer11@gmail.com>
2017-10-19 20:55:00 -0300

    se agrega negative lookahead en el match del tipo float

--- src/parser/Tokens.ts
+++ src/parser/Tokens.ts
@@ -61,7 +61,8 @@ export const tokens = {
   // Atoms
   hex:          { match: /0[xX][0-9a-f-A-F]+/, value: (x: string) => (parseInt(x,16)) },
   //float:        { match: /[0-9]+(?:\.[0-9])*(?:[eE][-+]?[0-9]+)?/, value: (x: string) => (parseFloat(x)) },
-  float:       { match: /[0-9]+(?:\.?[0-9]*(?:[eE][-+]?[0-9]+)?)?/, value: (x: string) => (parseFloat(x)) },
+  //float:       { match: /[0-9]+(?:\.?[0-9]*(?:[eE][-+]?[0-9]+)?)?/, value: (x: string) => (parseFloat(x)) },
+  float:        { match: /[0-9]+(?:\.?(?!\.)[0-9]*(?:[eE][-+]?[0-9]+)?)?/, value: (x: string) => (parseFloat(x)) },
   integer:      { match: /[0-9]+/, value: (x: string) => (parseFloat(x)) },
   literal:      { match: /\"[^"\\\n]*(?:\\.[^"\\\n]*)*\"/, value: (x: string) => (x)}, // Strings
   identifier:   /[a-zA-Z_][a-zA-Z0-9_]*/,
