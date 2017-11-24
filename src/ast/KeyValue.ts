32b059fa0d2dbb168693d090e9b5eb03b8c9a6cf
Eduardo Pérez <eduardo.perez.2151@gmail.com>
2017-11-24 00:58:57 -0300

    fix en key value se agregan nuevos test

--- src/ast/KeyValue.ts
+++ src/ast/KeyValue.ts
@@ -1,25 +1,34 @@
 import {Exp, Stmt} from './ASTNode';
 import {State} from '../interpreter/State';
+import {ErrorTypeInfo} from "./ErrorTypeInfo";
 
 export class KeyValue implements Exp {
 
-  id: string;
-  exp: Exp;
+    id: Exp;
+    exp: Exp;
 
-  constructor(id: string, exp: Exp) {
-    this.id = id;
-    this.exp = exp;
-  }
+    constructor(id: Exp, exp: Exp) {
+        this.id = id;
+        this.exp = exp;
+    }
 
-  toString(): string {
-    return `KeyValue(${this.id}, ${this.exp.toString()})`;
-  }
+    toString(): string {
+        return `KeyValue(${this.id}, ${this.exp.toString()})`;
+    }
 
-  unParse(): string {
-    return `${this.id} : ${this.exp.unParse()}`;
-  }
+    unParse(): string {
+        return `${this.id} : ${this.exp.unParse()}`;
+    }
 
-  evaluate(state: State): any {
-    return new KeyValue(this.id, this.exp.evaluate(state));
-  }
+    evaluate(state: State): any {
+        let identifier = id.evaluate(state);
+        if (typeof identifier === 'string') {
+            let keyValue= new Map();
+            keyValue.set('key',identifier);
+            keyValue.set('value',this.exp.evaluate(state));
+            return keyValue;
+        }
+        let error: [ErrorTypeInfo] = [new ErrorTypeInfo("exp", identifier)];
+        throw new EvalError(`\n######## ERROR DE TIPOS ########\n${error.join("\n") }\n################################`);
+    }
 }
--- test/QuaeroTest.ts
+++ test/QuaeroTest.ts
@@ -24,21 +24,36 @@ describe('Test Quaero:', () => {
     });
 
     test('Multiplication with function', () => {
-        let state:State = testUtil.executeInterpreter('mutiplication.qr');
+        let state:State = testUtil.executeInterpreter('simpleMul.qr');
         expect(state).not.toBeFalsy();
         let result=state.get('result');
         expect(result).not.toBeFalsy();
         expect(result).toEqual(25);
-
     });
 
     test('Division with function', () => {
-        let state:State = testUtil.executeInterpreter('division.qr');
+        let state:State = testUtil.executeInterpreter('simpleDiv.qr');
         expect(state).not.toBeFalsy();
         let result=state.get('result');
         expect(result).not.toBeFalsy();
         expect(result).toEqual(2);
-    
+    });
+
+    test('Factorial', () => {
+        let state:State = testUtil.executeInterpreter('factorial.qr');
+        expect(state).not.toBeFalsy();
+        let result1=state.get('result1');
+        let result2=state.get('result2');
+        let result3=state.get('result3');
+        let result4=state.get('result4');
+        expect(result1).not.toBeFalsy();
+        expect(result2).not.toBeFalsy();
+        expect(result3).not.toBeFalsy();
+        expect(result3).not.toBeFalsy();
+        expect(result1).toBe(1);
+        expect(result1).toEqual(result2);
+        expect(result3).toBe(120);
+        expect(result4).toBe(720);
     });
 
     test('Cartesian Product with List Comprenhention', () => {
